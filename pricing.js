66fb4114f9fbd0b93d4d55d83edd4e468db80c75
Cliff Lu <clifflu@gmail.com>
2013-06-13 23:31:05 +0000

    procing.js init

---
+++ pricing.js
@@ -0,0 +1,98 @@
+/**
+ * AWS ec2 pricing AMD module
+ * @param  {[type]} $){} [description]
+ * @return {[type]}        [description]
+ */
+//define('ec2-pricing', ['jquery'], 
+pricing = (function ($){
+    var _data, 
+        _xhr;
+
+    /**
+     * Filter output, returns a promise object
+     * 
+     * @param  {[type]} cond [description]
+     * @return {[type]}      [description]
+     */
+    function filter(cond) {
+        var out, payload;
+
+        if ("undefined" === typeof cond )
+            return _xhr;
+
+        out = new $.Deferred();
+
+        _xhr.done(function(d){
+            payload = _filter((d), cond);
+            out.resolve(payload);
+        })
+
+        return out.promise();
+    }
+
+    /**
+     * the blocking filter
+     * 
+     * @param  {[type]} d [description]
+     * @param  {[type]} cond [description]
+     * @return {[type]}      [description]
+     */
+    function _filter(d, cond) {
+        var output = {};
+
+        _filter_recursive(d.data, output, cond, 0);
+
+        return {
+            "sequence": d.sequence,
+            "tags": d.tags,
+            "data": output
+        };
+    }
+
+    function _in_filter(directive, filter_list) {
+        if ("undefined" == typeof filter_list) {
+            return true;
+        }
+
+        return (-1 !== filter_list.indexOf(directive));
+    }
+
+    function _filter_recursive(d, o, cond, depth) {
+        var i, my_filter;
+
+        if ("undefined" == typeof (_data.sequence[depth])) {
+            $.extend(o, d);
+            return;
+        }
+
+        my_filter = cond[_data.sequence[depth]];
+        if (!(
+            "[object Array]" === Object.prototype.toString.call(cond[_data.sequence[depth]]) ||
+            "undefined" === typeof cond[_data.sequence[depth]]
+        ))
+            my_filter = [cond[_data.sequence[depth]]];
+
+
+        for(i in d) {
+            if (!d.hasOwnProperty(i))
+                continue;
+
+            if (!_in_filter(i, my_filter))
+                continue;
+
+            o[i] = {};
+            _filter_recursive(d[i], o[i], cond, depth+1);
+        }
+    }
+
+    function init(){
+        _xhr = $.getJSON('pricing.php').
+            done(function(data){_data = data;});
+    }
+
+    init();
+
+    return {
+        filter: filter
+    };
+})(jQuery);
